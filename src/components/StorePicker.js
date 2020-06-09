c600b5e3c17cad816bc37937c7b1e2aa899ca86e
David Naimi <d88naimi@gmail.com>
2020-06-09 07:24:31 -0700

    added the router and not found page

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+    myInput = React.createRef();
+
+    goToStore = event => {
+        // 1. Stop the form from submitting
+        event.preventDefault();
+        // 2. get the text from that input
+        const storeName = this.myInput.current.value;
+        
+        // 3. Change the page to /store/whatever-they-entered
+        this.props.history.push(`/store/${storeName}`);
+      };
     render() {
         return (
         <React.Fragment> {/* NO adjacent jsx selector, React fragment allows for multiple elements in jsx */}
             <p> Form</p>
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.gotToStore}>
                 <h2>Please Enter A Store</h2>
-                <input type="text" required placeholder ="Store Name"/>
+                <input type="text" 
+                ref={this.myInput}
+                required 
+                placeholder ="Store Name"
+                defaultValue={getFunName()}
+                />
                 <button type="submit">Visit Store</button>
             </form>
         </React.Fragment>
